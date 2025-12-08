fa10dd6ffc40a0e50fcf5c8ac17554a702cefb31
Ivanilson Ferreira de Oliveira <ivanilson.ferreira.mec@gmail.com>
2025-12-08 15:27:32 -0300

    refactor(layout): reorganize global headers into layout/ and marketing/ structure

---
+++ components/layout/Header.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import HeroHeader from "../marketing/HeroHeader";
+import DefaultHeader from "../marketing/DefaultHeader";
+import PartnersHeader from "../marketing/PartnersHeader";
+
+export default function Header() {
+  const pathname = usePathname();
+
+  const isLanding =
+    pathname === "/" || pathname === "/index" || pathname === "";
+
+  const isPartners = pathname.startsWith("/partners");
+
+  if (isLanding) {
+    return <HeroHeader />;
+  }
+
+  if (isPartners) {
+    return <PartnersHeader />;
+  }
+
+  return <DefaultHeader />;
+}
